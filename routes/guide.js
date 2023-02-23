3d238560a70ed4a4a31ba42c54f7c7f7ff980ef6
vishnusp125 <103351267+vishnusp125@users.noreply.github.com>
2023-02-23 18:49:23 +0530

    nodemon

--- routes/guide.js
+++ routes/guide.js
@@ -7,11 +7,11 @@ import { guideProtect } from '../middleware/authMiddleware.js';
 router.post('/guidesignup', Guidesignup);
 router.post('/guidesignin', Guidesignin);
 router.post('/activityPost',guideProtect, activityPost);
-router.post('/languagePost',guideProtect, languagePost);
+router.post('/languagePost',guideProtect, languagePost);   
 router.get('/guideHome/:id',guideProtect,guideDetails)
-router.post('/pricePost',guideProtect, pricePost);
+router.post('/pricePost',guideProtect, pricePost);        
 router.post('/descriptionPost',guideProtect, descriptionPost);
-router.get('/guideBookings/:id',guideProtect, guideBookings);
+router.get('/guideBookings/:id',guideProtect, guideBookings);  
 router.post('/editProfile/:id', guideProtect, editProfile)
 router.delete('/:guideId/activities/:activityIndex', guideProtect, activityDelete)
 router.delete('/:guideId/languages/:languageIndex', guideProtect, languageDelete)
