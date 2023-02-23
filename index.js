eba0d7d9efdd81bc34b8fa6325ad77ef9635ae3f
vishnusp125 <103351267+vishnusp125@users.noreply.github.com>
2023-02-23 13:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
 const io = new Server(server, {
   pingTimeout: 60000,
-  cors: {
-    origin: "https://zippy-profiterole-14fc38.netlify.app/",
-    cors:true,
-  },
+  // cors: {
+    // origin: "https://zippy-profiterole-14fc38.netlify.app/",
+    // cors:true,
+  // },
 });
 
 let users = [];
