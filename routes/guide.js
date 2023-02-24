efe70d2508d42638b49f52112382597ac5e498a3
vishnusp125 <103351267+vishnusp125@users.noreply.github.com>
2023-02-24 09:10:41 +0530

    after some changes

--- routes/guide.js
+++ routes/guide.js
@@ -8,13 +8,13 @@ router.post('/guidesignup', Guidesignup);
 router.post('/guidesignin', Guidesignin);
 router.post('/activityPost',guideProtect, activityPost);
 router.post('/languagePost',guideProtect, languagePost);   
-router.get('/guideHome/:id',guideProtect,guideDetails)
+router.get('/guideHome/:id',guideProtect,guideDetails)   
 router.post('/pricePost',guideProtect, pricePost);        
 router.post('/descriptionPost',guideProtect, descriptionPost);
 router.get('/guideBookings/:id',guideProtect, guideBookings);  
 router.post('/editProfile/:id', guideProtect, editProfile)
 router.delete('/:guideId/activities/:activityIndex', guideProtect, activityDelete)
-router.delete('/:guideId/languages/:languageIndex', guideProtect, languageDelete)
+router.delete('/:guideId/languages/:languageIndex', guideProtect, languageDelete)  
 
 
 
